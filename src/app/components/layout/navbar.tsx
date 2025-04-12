fec647324e950e64f8028ea54d56596944b5edb7
luisregardiz <gadeeks@gmail.com>
2025-04-12 13:22:41 -0400

    feat:Add Navbar component and update layout with new styles and favicon

---
+++ src/app/components/layout/navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import type { FC } from "react";
+
+export const Navbar: FC = () => {
+    return (
+        <header>
+            <nav className="h-20 flex items-center justify-center">
+                <div className="container mx-auto flex justify-between items-center">
+                    <div className="text-white text-lg font-semibold">
+                        <Link href="/">Chuck Norris Random Joke</Link>
+                    </div>
+                    <ul className="flex space-x-4">
+                        <li>
+                            <Link
+                                href="/"
+                                className="text-white hover:text-gray-400"
+                            >
+                                New Joke
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/favorites"
+                                className="text-white hover:text-gray-400"
+                            >
+                                Favorites
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
---
+++ src/app/favorites/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function User() {
+    return (
+        <div>
+            <div className="mt-4">Chuck Norris Joke User</div>
+        </div>
+    );
+}
